2a7dd4627d7aa425440124c1f7b1ba0a8197717a
illiaNaumov <ilyuadier@gmail.com>
2022-02-21 20:42:51 +0200

    initial commit

--- app/src/main/java/com/naumov/currencyrate/Constant.kt
+++ app/src/main/java/com/naumov/currencyrate/Constant.kt
@@ -3,6 +3,6 @@ package com.naumov.currencyrate
 /**
  * Please set your app id here for https://docs.openexchangerates.org/
  */
-const val APP_ID: String = "b588ecef656f42bb8ca860b5669b971c"
+const val APP_ID: String = ""
 
 const val BASE_URL: String = "https://openexchangerates.org/api/"
